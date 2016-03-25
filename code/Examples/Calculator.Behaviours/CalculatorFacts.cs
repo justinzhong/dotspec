e6b9da091ea231881e202ff32e1e6d4614abd73b
Justin Zhong <justin@getmybiz.com.au>
2016-03-25 13:57:12 +1100

    Moved Calculator* projects to under Examples folder outside of the Dotspec project folder.

--- code/Examples/Calculator.Behaviours/CalculatorFacts.cs
+++ code/Examples/Calculator.Behaviours/CalculatorFacts.cs
@@ -50,24 +50,6 @@ namespace Calculator.Behaviours
             }
         }
 
-        ///// <summary>
-        ///// Specifies the theories on the Calculator's add behaviour.
-        ///// </summary>
-        //private IEnumerable<Spec<Calculator>> AddBehaviourSpecData
-        //{
-        //    get
-        //    {
-        //        yield return "1. When adding two identical integers a, the result should be (2 x a).".Spec<Calculator>()
-        //            .Given(new int[] { 21, 21, (2 * 21) });
-
-        //        var a = _random.Next(int.MaxValue / 2);
-        //        var b = _random.Next(int.MaxValue / 2);
-
-        //        yield return "2. When adding two random integers a and b, the result should yield the sum of a and b.".Spec<Calculator>()
-        //            .Given(new int[] { a, b, (a + b) });
-        //    }
-        //}
-
         /// <summary>
         /// Asserts the Calculator's add behaviour.
         /// </summary>
@@ -76,16 +58,5 @@ namespace Calculator.Behaviours
         {
             AddBehaviourInlineSpecs.Assert(_subject);
         }
-
-        //public void AssertAddBehaviourWithSpecData()
-        //{
-        //    var addBehaviourSpec = "Add behaviour spec".Spec<Calculator>()
-        //        .Given(new int[] { })
-        //        .When(
-        //            (calculator, data) => calculator.Add(data[0], data[1]))
-        //        .Then((data, result) => result.ShouldBe(data[2]));
-
-        //    //addBehaviourSpec.Assert(_subject, AddBehaviourSpecData);
-        //}
     }
 }
